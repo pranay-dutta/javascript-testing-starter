7f5becc80f2a45d7572da3dcc19b6819cafa7d7b
Pranay Dutta <rick15176@gmail.com>
2025-05-20 01:24:56 +0530

    Keep calm and commit

---
+++ src/test/main.test.ts
@@ -0,0 +1,12 @@
+import { it, expect, describe } from "vitest";
+import { calculateDiscount } from "../main";
+
+describe("calculateDiscount", () => {
+  it("should return invalid if first arg is neg or not number", () => {
+    expect(calculateDiscount(0, "")).toMatch(/invalid/i); //not reqiured in ts
+  });
+  it("should return valid discount", () => {
+    expect(calculateDiscount(10, "SAVE10")).toBe(9);
+    expect(calculateDiscount(10, "SAVE20")).toBe(8);
+  });
+});
